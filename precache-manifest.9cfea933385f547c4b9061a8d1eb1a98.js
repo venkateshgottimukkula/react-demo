4183d7c035ad8e16c067719d6d7394da69ab88fe
Venkatesh Gottimukkula <venkatesh.g@knexinc.com>
2020-08-05 19:31:16 +0530

    Updates

---
+++ precache-manifest.9cfea933385f547c4b9061a8d1eb1a98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a18eac131f3c93400bcac7a5067fbf",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "5a3414b86dcbc3c380f6",
+    "url": "/react-demo/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "da9421229b46da974eec",
+    "url": "/react-demo/static/css/main.ff580565.chunk.css"
+  },
+  {
+    "revision": "5a3414b86dcbc3c380f6",
+    "url": "/react-demo/static/js/2.c7172e81.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-demo/static/js/2.c7172e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9421229b46da974eec",
+    "url": "/react-demo/static/js/main.3105e5ae.chunk.js"
+  },
+  {
+    "revision": "9e95bcee3ee2cd82c94f",
+    "url": "/react-demo/static/js/runtime-main.f7dacf01.js"
+  }
+]);
